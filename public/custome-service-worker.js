51eaf66f5a8addc92244b3243d515160da8701eb
VictoryTheDragon <ka_ivj@live.com>
2020-02-26 10:30:59 -0600

    Error de import resuelto creo

--- public/custome-service-worker.js
+++ public/custome-service-worker.js
@@ -1,5 +1,7 @@
 
-const serverSocket = require('socket.io-client')
+import serverSocket from '../node_modules/socket.io-client'
+//const serverSocket = 
+//const serverSocket = require('../node_modules/socket.io-client');
 const endPoint = 'https://instrumentacionline.ddns.net/';
 const socket = serverSocket(endPoint);
 socket.on('temp',(temp)=>{
