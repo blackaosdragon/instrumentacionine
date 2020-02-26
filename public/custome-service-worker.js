9af9376fda29def517477ce51e7059c5ac9f0597
VictoryTheDragon <ka_ivj@live.com>
2020-02-26 10:15:30 -0600

    Ahora en lugar de import uso require

--- public/custome-service-worker.js
+++ public/custome-service-worker.js
@@ -1,12 +1,11 @@
 
-function monitorear(){
-    import serverSocket from 'socket.io-client';
-    const endPoint = 'https://instrumentacionline.ddns.net/';
-    const socket = serverSocket(endPoint);
-    socket.on('temp',(temp)=>{
+const serverSocket = require('socket.io-client')
+const endPoint = 'https://instrumentacionline.ddns.net/';
+const socket = serverSocket(endPoint);
+socket.on('temp',(temp)=>{
     console.log(temp);
 })
-}
+
 
 self.addEventListener('load',()=>{
     console.log("!!!!!!!");
