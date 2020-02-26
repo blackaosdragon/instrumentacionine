ee21d43d2da636caf302fa46965033fe630bc202
VictoryTheDragon <ka_ivj@live.com>
2020-02-26 13:14:02 -0600

    Segunda version intentando ller la pagina

--- public/custome-service-worker.js
+++ public/custome-service-worker.js
@@ -1,4 +1,4 @@
-
+/*
 import serverSocket from '../node_modules/socket.io-client'
 //const serverSocket = 
 //const serverSocket = require('../node_modules/socket.io-client');
@@ -6,6 +6,12 @@ const endPoint = 'https://instrumentacionline.ddns.net/';
 const socket = serverSocket(endPoint);
 socket.on('temp',(temp)=>{
     console.log(temp);
+})
+*/
+fetch('https://instrumentacionline.ddns.net').then(function(response){
+    console.log(response);
+}).catch(function(err){
+    console.log(err);
 })
 
 
