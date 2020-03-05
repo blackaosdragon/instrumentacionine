7e24372de4d11cdfa91284bb49ecd6310b084e51
VictoryTheDragon <ka_ivj@live.com>
2020-03-05 12:03:07 -0600

    PWA intento de instalacion

--- public/custome-service-worker.js
+++ public/custome-service-worker.js
@@ -1,31 +1,3 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/precache-manifest.57151917f87a4f1f6973d601d6714d94.js"
-);
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
-
-
 /*
 import serverSocket from '../node_modules/socket.io-client'
 //const serverSocket = 
