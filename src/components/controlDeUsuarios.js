3a9198dcac230740caf342f3c509c56772c086c5
VictoryTheDragon <ka_ivj@live.com>
2020-07-24 17:55:43 -0500

    Pruebas para revertir la cadena

--- src/components/controlDeUsuarios.js
+++ src/components/controlDeUsuarios.js
@@ -1,23 +1,65 @@
 import React, {Component} from 'react';
 import { TextField } from '@material-ui/core';
+import CryptoJS from 'crypto-js';
+import styled from 'styled-components'
+import {withStyles,createMuiTheme} from '@material-ui/core/styles';
+
+let estado_campo = 'blue'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+             main: '#003366'
+
+        },
+        secondary: {
+            main: '#ff0000'
+        }
+    }
+})
+const TextPersonal = styled(TextField)`
+  label.Mui-focused{
+      color: blue;
+  }
+  & .MuiInput-underline: {
+      borderBotomColor: yellow
+  }
+`;
+
+const PersonalTextoField = withStyles({
+    root: {
+        '& label.Mui-focused': {
+            color: estado_campo,
+        },
+        '.MuiInput-underline:after': {
+            borderBottomColor: estado_campo
+        },
+        '& .label' : {
+            color: 'yellow'
+        }
+    }
+})(TextField);
+
+
 
 class ControlUsers extends Component{
     constructor(props){
         super(props);
         this.state = {
             usuario: '',
+            oirausu: '',
             contraseña: '',
-            hasch: ''
-        }
-        this.handleListener = this.handleListener.bind(this);
-    }
-    handleListener = () => {
-        if(this.state.contraseña.length>5){
-            let salt = crypto.randomBytes(128).toString('base64');
-            this.setState({
-                ...this.state,
-                hasch: ""
-            })
+            hasch: '',
+            error_usuario: false,
+            error_password: false,
+            error_personal: false,
+            leyenda_usuario: "Ingresa tu usuario",
+            campA: {
+                error: false,
+                value: '',
+                helper: 'Ingresa el usuario'
+            }
+            
         }
     }
     componentDidMount(){
@@ -31,7 +73,61 @@ class ControlUsers extends Component{
         this.setState({
             [event.target.name]: event.target.value
         })
-        console.log(event.target)
+        
+        //console.log(event.target.name);
+        console.log(event.target.value.length);
+    }
+    onBlur = (event) => {
+        //console.log(this.state);
+        console.log(event.target);
+        console.log(event.target.name);
+        console.log(event.target.value);
+        console.log(CryptoJS.SHA3(event.target.value).toString(CryptoJS.enc.Hex));
+        console.log(CryptoJS.SHA3(event.target.value).toString(CryptoJS.enc.Base64));
+        if(event.target.value.length>=4){
+            this.setState({
+                //error_usuario: true
+            })
+        } else {
+            this.setState({
+                //error_usuario: false
+            })
+        }
+        let cadena = [];
+        for(let i = 0; event.target.value.length>i;i++){
+            console.log(event.target.value.length);
+            
+            if(i==0){
+                i=i+1;
+            }
+            
+            cadena[i] = event.target.value[event.target.value.length-i]
+            
+            /*this.setState({
+
+            })*/
+        }
+        console.log(cadena)
+        
+    }
+    onBlurHandle = (event) => {
+        console.log(event.target.value.length);
+        if (event.target.value.length<=6){
+            console.log(estado_campo);
+            this.setState({
+                leyenda_usuario: 'Usuario muy corto',
+                error_personal: true
+            })
+            estado_campo = 'red';
+            console.log(estado_campo);
+        } else if (event.target.value.length>6){
+            console.log("Usuario correcto");
+            this.setState({
+                leyenda_usuario: 'Campo correcto',
+                error_personal: false
+            })
+            
+        }
     }
     enviar = () => {
          
@@ -48,14 +144,19 @@ class ControlUsers extends Component{
                         Login
                         <div className="textos">
                           <TextField
+                                                     
                            name="usuario"
                            label="Usuario"
                            helperText="Ingrese su e-mail/usuario"
                            value={this.state.usuario}
                            onChange={this.onChange}
+                           onBlur={this.onBlur}
                            fullWidth
+                           //error={this.state.error_usuario}
                            />
+                           
                            <TextField
+                           onBlur={this.onBlur}
                             name="contraseña"
                              label="Contraseña"
                              helperText="Ingrese su contraseña"
@@ -64,6 +165,23 @@ class ControlUsers extends Component{
                              type='password'
                              fullWidth
                            />
+                           {/*
+                           <PersonalTextoField
+                             fullWidth
+                             label="Prueba"
+                             helperText={this.state.leyenda_usuario}
+                             onBlur={this.onBlurHandle}
+                             error={this.state.error_personal}
+                             />
+                             <TextPersonal
+                             fullWidth
+                             label="Segunda prueba"
+                             helperText={this.state.leyenda_usuario}
+                             onBlur={this.onBlurHandle}
+                             
+                             //error={this.state.error_personal}
+                             />
+                             */}
                         </div>
                         
                          </div>
