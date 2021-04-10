df76058119becd18d495137dca29f57f1ea661cf
Isaac Victoria <ka_ivj@live.com>
2021-04-10 02:35:19 -0500

    Actualizada la página porque las imagenes en desktop se veian muy mal

--- src/components/cargaImagen.js
+++ src/components/cargaImagen.js
@@ -10,30 +10,7 @@ class CargaImagen extends Component{
         }
     }
     componentDidMount(){
-        // if(this.state.cargando===1){
-        //     //console.log("ESta cargando")
-        //     setTimeout(()=>{
-        //         this.setState({
-        //             cargando: 0
-        //         })
-        //     },2500)
-        // }
-        // fetch('https://instrumentacionline.ddns.net:5002/imagenes/mesas1.jpg',
-        // {
-        //     method: 'GET',
-        //     mode: 'cors',
-        //     cache: 'no-cache',
-        //     headers:{
-        //         'Content-Type': 'image/jpeg' 
-        //     }
-        // }
-        // )
-        // .then( response => {
-        //     console.log(response)
-        //     //response.blob()            
-        // }).catch( err => {
-        //     console.log(err.message)
-        // })
+        
     }
     componentDidUpdate(prevProps,prevState){
         //console.log(this.props)
@@ -47,48 +24,32 @@ class CargaImagen extends Component{
     }
     
     render(){
-        /*
-        let imagen = ''
-        console.log(this.props)
-        console.log(this.state)
-        
-       
-        if(this.props.anchura>970){
-            {
-                this.state.cargando ? 1 :
-                <Carga cargando={this.state.cargando} />
-            }
-            imagen = <img onLoad={()=>{this.setState({cargando: 0})}} src={`${this.props.direccion}`} className="vistaMesas"/>
-            // if(this.state.cargando===1){
-            //     imagen = <Carga cargando={this.state.cargando}/>
-            // } else {
-            //     imagen = <img onLoad={()=>{console.log("Entendiendo que carga")}} src={`${this.props.direccion}`} className="vistaMesas"/>
-            // }
-             
-            
+        const { anchura } = this.props
+        if(anchura>970){
+            return(
+                <Fragment>
+                    {
+                        this.state.cargando ? 1 :
+                        <Carga cargando={this.state.cargando} />
+                    }
+                    <img onLoad={()=>{this.setState({cargando: 1})}} src={`${this.props.direccion}`} className="vistaMesasDesktop"/>
+                </Fragment>
+            )
         } else {
-            if(this.state.cargando===1){
-                imagen = <Carga cargando={this.state.cargando}/>
-            } else {
-                imagen = <img onLoad={()=>{console.log("Entendiendo que carga")}} src={`${this.props.direccion}`} className="vistaMesas"/>
-            }
-            
-            
-        }  
-        */
+            return(
+                <Fragment>
+    
+                    {
+                        this.state.cargando ? 1 :
+                        <Carga cargando={this.state.cargando} />
+                    }
+                    <img onLoad={()=>{this.setState({cargando: 1})}} src={`${this.props.direccion}`} className="vistaMesas"/>
+                    
+                </Fragment>
+                
+            )
+        }
         
-        return(
-            <Fragment>
-
-                {
-                    this.state.cargando ? 1 :
-                    <Carga cargando={this.state.cargando} />
-                }
-                <img onLoad={()=>{this.setState({cargando: 1})}} src={`${this.props.direccion}`} className="vistaMesas"/>
-                {/* {imagen} */}
-            </Fragment>
-            
-        )
     }
 }
 export default CargaImagen;
