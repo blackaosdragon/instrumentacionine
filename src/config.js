2a65935007ccf73601f44b966969e5cd8122deea
blackaosdragon <ka_ivj@live.com>
2021-07-07 17:56:57 -0500

    Ultimo cambio ya que las rutas cambiaron un poco

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 let develop
 export default develop = {
-    API_URL: 'https://instrumentacionline.ddns.net',
-    API_URL_MASTER: 'https://instrumentacionline.ddns.net',
+    API_URL: 'https://instrumentacion.ddns.net',
+    API_URL_MASTER: 'https://instrumentacion.ddns.net',
 }
 /*
 export const master = {
