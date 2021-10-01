5a9d33ee42dc54560086ed8ae9b11115e664f4b6
blackaosdragon <ka_ivj@live.com>
2021-10-01 16:16:43 -0500

    Cambios al footer porque esta quedando descuadrado

--- src/components/footer.js
+++ src/components/footer.js
@@ -38,7 +38,10 @@ class Footer extends Component{
           <div>
             <p className="titulos">Nuestras oficinas se encuentran en la calle Francisco Tamagno #32 en la Colonia Ex Hipódromo de Peralvillo, Ciudad de México </p>
             <p className="titulos">Si tiene alguna duda, sugerencia o tiene algún mal funcionamiento de la página favor de mandar un correo a contacto@intrumentaciononline.com.mx</p>
-            <p className="titulos" ><ListItem style={{margin: "0% 0% 0% 25%"}}><Whats /> (52) 55 55 83 91 82</ListItem ><ListItem style={{margin: "0% 0% 0% 15%"}}><Mail /> instrumentacion_@hotmail.com</ListItem ></p>
+            <p className="titulos" ><Whats /> (52) 55 55 83 91 82</p>
+            <p className="titulos" ><Mail /> instrumentacion_@hotmail.com</p>
+            
+            
           </div>
         )
 
